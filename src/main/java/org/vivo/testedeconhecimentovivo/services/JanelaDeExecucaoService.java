1cca2a8f027e01b2359463cc50bbd9be3bcdfa51
Lucas Gonçalves <lucasgoncalves.jobs@gmail.com>
2022-01-05 01:17:41 -0300

    feat: implementa filtragem de jobs que estão na janela de execução

--- src/main/java/org/vivo/testedeconhecimentovivo/services/JanelaDeExecucaoService.java
+++ src/main/java/org/vivo/testedeconhecimentovivo/services/JanelaDeExecucaoService.java
@@ -3,11 +3,22 @@ package org.vivo.testedeconhecimentovivo.services;
 import org.vivo.testedeconhecimentovivo.models.Job;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class JanelaDeExecucaoService {
 
     public List<List<Integer>> execute(List<Job> jobs, LocalDateTime dataInicio, LocalDateTime dataFim) {
-        return null;
+
+        List<List<Integer>> resultado = new ArrayList<>();
+
+
+        List<Job> jobsFiltrado = jobs.stream().filter(job -> !(job.getDataMaximaConclusao().isAfter(dataInicio) || job.getDataMaximaConclusao().isEqual(dataInicio)) &&
+                !(job.getDataMaximaConclusao().isBefore(dataFim) || job.getDataMaximaConclusao().isEqual(dataFim))).collect(Collectors.toList());
+
+
+
+        return resultado;
     }
 }
