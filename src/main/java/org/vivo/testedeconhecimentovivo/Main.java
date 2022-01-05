bbe2f27c9945c5cfb1feb8b4a1467462df1d037d
Lucas Gonçalves <lucasgoncalves.jobs@gmail.com>
2022-01-05 00:20:20 -0300

    feat: criação do main, inserção da massa de dados

---
+++ src/main/java/org/vivo/testedeconhecimentovivo/Main.java
@@ -0,0 +1,39 @@
+package org.vivo.testedeconhecimentovivo;
+
+import org.vivo.testedeconhecimentovivo.models.Job;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        Job job1 = new Job(1,
+                "Importação de arquivos de fundos",
+                LocalDateTime.parse("2019-11-10 12:00:00", formatter),
+                2
+        );
+
+        Job job2 = new Job(2,
+                "Importação de dados da Base Legada",
+                LocalDateTime.parse("2019-11-11 12:00:00", formatter),
+                4
+        );
+
+        Job job3 = new Job(3,
+                "Importação de dados de integração",
+                LocalDateTime.parse("2019-11-11 08:00:00", formatter),
+                6
+        );
+
+        List<Job> jobs = List.of(job1, job2, job3);
+
+        LocalDateTime dataInicio = LocalDateTime.parse("2019-11-10 09:00:00", formatter);
+        LocalDateTime dataFim = LocalDateTime.parse("2019-11-11 12:00:00", formatter);
+
+    }
+
+}
