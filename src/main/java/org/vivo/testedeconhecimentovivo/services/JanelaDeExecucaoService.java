5d1bc576a9da0165d15f72d436337c7871b0dbac
Lucas Gonçalves <lucasgoncalves.jobs@gmail.com>
2022-01-05 02:16:51 -0300

    feat: implementação da execução finalizada

--- src/main/java/org/vivo/testedeconhecimentovivo/services/JanelaDeExecucaoService.java
+++ src/main/java/org/vivo/testedeconhecimentovivo/services/JanelaDeExecucaoService.java
@@ -5,14 +5,14 @@ import org.vivo.testedeconhecimentovivo.models.Job;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 import java.util.stream.Collectors;
 
 public class JanelaDeExecucaoService {
 
     public List<List<Integer>> execute(List<Job> jobs, LocalDateTime dataInicio, LocalDateTime dataFim) {
 
-        List<List<Integer>> resultado = new ArrayList<>();
-
+        Stack<List<Job>> stackDeExecucao = new Stack<>();
 
         List<Job> jobsFiltrado = jobs.stream().filter(job ->
                         (job.getDataMaximaConclusao().isAfter(dataInicio) || job.getDataMaximaConclusao().isEqual(dataInicio)) &&
@@ -20,10 +20,25 @@ public class JanelaDeExecucaoService {
                 ).sorted((job1, job2) -> job1.getDataMaximaConclusao().compareTo(job2.getDataMaximaConclusao()))
                 .collect(Collectors.toList());
 
-        System.out.println(jobsFiltrado);
+        jobsFiltrado.forEach(job -> {
+            if (stackDeExecucao.isEmpty()) {
+                List<Job> listJobs = new ArrayList<>();
+                listJobs.add(job);
+                stackDeExecucao.push(listJobs);
+            } else {
+                List<Job> listJobsInStack = stackDeExecucao.peek();
+                int tempoEstimadoTotal = listJobsInStack.stream().mapToInt(Job::getTempoEstimadoEmHoras).sum();
+                if(tempoEstimadoTotal + job.getTempoEstimadoEmHoras() <= 8) {
+                    listJobsInStack.add(job);
+                } else {
+                    List<Job> listJobs = new ArrayList<>();
+                    listJobs.add(job);
+                    stackDeExecucao.push(listJobs);
+                }
+            }
+        });
 
+        return stackDeExecucao.stream().map(listJobs -> listJobs.stream().map(Job::getId).collect(Collectors.toList())).collect(Collectors.toList());
 
-
-        return null;
     }
 }
